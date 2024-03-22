bf259877d2e361c94d6a1a3b6f7c302a743163bf
hasansino <4024450+hasansino@users.noreply.github.com>
2024-03-22 22:53:14 +0200

    simple concatenate

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello World!")
+def main(x, y):
+    print("Hello, this is a basic Python program." + " " + y + " " + str(x))
+
+
+if __name__ == "__main__":
+    x = 53
+    y = "test"
+    main(x, y)
